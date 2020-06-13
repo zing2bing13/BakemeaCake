6e292924d4c50d00511e9fd562731897cfd76d05
kheimon <ej@theros.ca>
2020-06-13 12:11:02 -0700

    Added text/html sharing options

--- app/src/main/java/com/example/bakemeacake/util/sharing/Share.java
+++ app/src/main/java/com/example/bakemeacake/util/sharing/Share.java
@@ -6,32 +6,73 @@ import android.net.Uri;
 import android.support.v4.app.ShareCompat;
 import android.support.v4.content.FileProvider;
 
+import com.example.bakemeacake.R;
+
 import java.io.File;
 
 public class Share extends Activity {
     String FILES_AUTHORITY = "com.example.bakemeacake.fileprovider";
+    String shareTitle;
+    File shareFile;
+    Intent shareIntent;
+    Uri shareUri;
 
-    public void sendShareText() {
+    public void sendShareText(String shareText) {
+        try {
+            shareIntent = ShareCompat.IntentBuilder.from(this)
+                            .setType("text/plain")
+                            .setText(shareText)
+                            .getIntent();
 
+            if (shareIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(shareIntent);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendShareHTML(String shareHtmlSubject,
+                              String shareHtmlText,
+                              String shareHtmlTo) {
+        try {
+            shareIntent = ShareCompat.IntentBuilder.from(this)
+                            .setType("text/html")
+                            .setText(shareHtmlText)
+                            .setSubject(shareHtmlSubject)
+                            .addEmailTo(shareHtmlTo)
+                            .getIntent();
+
+            if (shareIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(shareIntent);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void sendShareFile(String filepath) {
         try {
-            File file = new File(filepath);
-            Uri uriToImage = FileProvider.getUriForFile(
-                    getApplicationContext(),
-                    FILES_AUTHORITY,
-                    file);
+            shareTitle = getString(R.string.share_generic);
+            shareFile = new File(filepath);
+            shareUri = FileProvider.getUriForFile(
+                            getApplicationContext(),
+                            FILES_AUTHORITY,
+                            shareFile
+                        );
 
-            Intent shareIntent =
-                    ShareCompat.IntentBuilder.from(this).setStream(uriToImage).getIntent();
+            shareIntent = ShareCompat.IntentBuilder.from(this)
+                            .setStream(shareUri)
+                            .getIntent();
 
             // Provide read access to target
-            shareIntent.setData(uriToImage);
+            shareIntent.setData(shareUri);
             shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
             // Start the Share Intent
-            startActivity(Intent.createChooser(shareIntent,"Share image to..."));
+            if (shareIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(Intent.createChooser(shareIntent,shareTitle));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }

0a258b441c5f591378a8e2fc574e42b51fe38efd
Leon Zhen <leonzhen@gmail.com>
2020-06-12 20:40:39 -0700

    Add models for Recipe, Ingredients, and Instruction Added DB CRUD for Recipe, Ingredients, and Instruction

---
+++ app/src/main/java/com/example/bakemeacake/data/model/Ingredient.java
@@ -0,0 +1,8 @@
+package com.example.bakemeacake.data.model;
+
+public class Ingredient {
+    public int ID;
+    public String Name;
+    public int Amount;
+    public int Recipe_ID;
+}
---
+++ app/src/main/java/com/example/bakemeacake/data/model/Instruction.java
@@ -0,0 +1,8 @@
+package com.example.bakemeacake.data.model;
+
+public class Instruction {
+    public int ID;
+    public int Recipe_ID;
+    public int Step;
+    public String Instruction;
+}
